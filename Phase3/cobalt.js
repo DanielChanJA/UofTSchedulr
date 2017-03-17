7aac557947b41e021c063a6d35a9d8cc9dbccb03
Daniel Chan <iam@danielchan.me>
2017-03-17 00:10:41 -0400

    Get Course is working.

---
+++ Phase3/cobalt.js
@@ -0,0 +1,5 @@
+var request = require('request');
+
+var cobaltKey = "wxyV572ztbmjVEc7qcokZ0xYVPv2Qf0n";
+var cobaltApi = "https://cobalt.qas.im/api/1.0/";
+
--- Phase3/index.js
+++ Phase3/index.js
@@ -2,8 +2,12 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongo = require("mongodb").MongoClient;
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+//Stuff to pull from Cobalt API.
+var request = require('request');
 var cobalt = "?key=wxyV572ztbmjVEc7qcokZ0xYVPv2Qf0n";
+var cobaltApi = "https://cobalt.qas.im/api/1.0/courses/";
 
 app.use(bodyParser.json());
 
@@ -18,18 +22,35 @@ mongo.connect("mongodb://localhost:27017/schdule", function(err, db) {
 });
 
 
-// Search for course
-app.get("/index", function(req, res) {
-    var cobaltUrl = cobalt;
-    xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log(JSON.parse(xhr.responseText));
+function getCourse(req, res) {
+    var query = req.query.course;
+    console.log("Searched for: " + query);
+
+    // 10 results by default. Use ?limit=x to get more/less, up to a maximum of 100.
+    var searchUri = cobaltApi + "search" + cobalt + "&q=" + query;
+    console.log("URI: " + searchUri);
+
+    request(searchUri, function(err, resp, body) {
+        if (err) {
+            console.log("Error:", err);
         }
-    }
-    xhr.open("GET", cobaltUrl, true);
-    xhr.send("");
-});
+
+        if (resp.statusCode != 200) {
+            console.log("Invalid status code: ", resp.statusCode);
+        }
+
+        // Everything is a-ok.
+        res.send(JSON.parse(body));
+
+    });
+
+}
+
+function generateTimetable(req, res) {
+
+}
+
+
 
 
 // Save timetable
@@ -41,5 +62,10 @@ app.put("/index", function(req, res) {
 
 // Delete timetable
 app.delete("/index", function(req, res) {
-    
+
 });
+
+
+app.get('/search', getCourse);
+app.post('/login', processLogin);
+app.get('/timetable', generateTimetable);
