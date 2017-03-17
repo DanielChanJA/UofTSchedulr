1bb98ceda5f8d849f7caae0655263f06563a771d
Daniel Chan <iam@danielchan.me>
2017-03-17 02:00:52 -0400

    Insert into DB with post req.

--- Phase3/index.js
+++ Phase3/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
+var app = express();
 var mongoose = require("mongoose");
 var mongo = require("mongodb").MongoClient;
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -9,6 +9,14 @@ var mongo = require("mongodb").MongoClient;
 var request = require('request');
 var cobalt = "?key=wxyV572ztbmjVEc7qcokZ0xYVPv2Qf0n";
 var cobaltApi = "https://cobalt.qas.im/api/1.0/courses/";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
 
 mongoose.connect("mongodb://localhost/schdule", function(err, db) {
     if (err) {
@@ -23,14 +31,20 @@ mongoose.connect("mongodb://localhost/schdule", function(err, db) {
 
 var courseSchema = new mongoose.Schema({
     userid: String,
-    courseid: String
+    courseid: String,
+    lecture: String
 });
-
 
 var Course = mongoose.model("Course", courseSchema);
 
 
-app.use(bodyParser.json());
+var timetableId = new mongoose.Schema({
+    userid: String,
+    courseid1: String,
+
+});
+
+
 
 
 function getCourse(req, res) {
@@ -59,41 +73,54 @@ function getCourse(req, res) {
 
 function insertCourse(req, res) {
 
-    var userid = req.query.userid;
-    var course = req.query.courseid + req.query.sem;
+    // res.json(req.body);
+    if (req.body.userid == null) {
+        console.log("Failed");
+        console.log(req.body);
+    }
+
+    var userid = req.body.userid;
+    var course = req.body.courseid + req.body.sem;
+    var section = req.body.lecture;
 
     console.log("Username: " + userid);
     console.log("Course: " + course);
+    console.log("Section: " + section);
 
 
-    Course.findOne({ "userid": userid, "courseid": course }, function(err, courseResult) {
+    Course.findOne({ "userid": userid, "courseid": course, "lecture": section }, function(err, courseResult) {
         if (err) {
             console.log("Error retrieving users.");
             return;
         }
 
-        if (courseResult.userid == null) {
+        if (courseResult == null) {
             Course.create({
                 "userid": userid,
-                "courseid": course
+                "courseid": course,
+                "lecture": section
             }, function(err, course) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log(course);
+                    console.log(courseResult);
                 }
             });
 
             console.log("Created entry");
-            res.status("200").end();
+            res.json({
+                Status: "Success",
+                Message: "Entry inserted into DB."
+            });
 
         } else {
-            console.log("DB contains entry: " + courseResult.userid + " : " + courseResult.courseid);
+            console.log("DB contains entry: " + courseResult.userid + " : " + courseResult.courseid + " : " + courseResult.lecture);
             res.send("Entry exists");
         }
     });
 
 }
+
 
 
 
