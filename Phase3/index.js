7788803d84f2e68ca8cb94043b4c41fdb2b0fd5f
George Lam <georgelam@George.local>
2017-03-17 22:41:24 -0400

    I think it works?

--- Phase3/index.js
+++ Phase3/index.js
@@ -33,6 +33,7 @@ var request = require('request');
 var cobalt = "?key=wxyV572ztbmjVEc7qcokZ0xYVPv2Qf0n";
 var cobaltApi = "https://cobalt.qas.im/api/1.0/courses/";
 
+var selectedCourses = [];
 
 /**
  * Initialization.
@@ -122,12 +123,13 @@ function insertCourse(req, res) {
     }
 
     var userid = req.body.userid;
-    var course = req.body.courseid + req.body.sem;
-    var section = req.body.lecture;
+//    var course = req.body.courseid + req.body.sem;
+//    var section = req.body.lecture;
+    var course = req.body.data[0].id;
 
     console.log("Username: " + userid);
     console.log("Course: " + course);
-    console.log("Section: " + section);
+//    console.log("Section: " + section);
 
 
     // Will not allow users to add multiple lecture sections of the same course. Only one is permitted.
@@ -140,11 +142,11 @@ function insertCourse(req, res) {
             });
         }
 
-        if (courseResult == null) {
+        if (courseResult == null && buildTimetable(req.body.data[0])) {
             Course.create({
                 "userid": userid,
                 "courseid": course,
-                "lecture": section
+//                "lecture": section
             }, function(err, course) {
                 if (err) {
                     console.log(err);
@@ -154,16 +156,24 @@ function insertCourse(req, res) {
             });
 
             console.log("Created entry");
+            
+            for (let i = 0; i < selectedCourses.length; i++) {
+                console.log(selectedCourses[i].days);
+                console.log(selectedCourses[i].timeslots);
+            }
+            
+            
             return res.json({
                 Status: "Success",
                 Message: "Entry inserted into DB."
             });
 
         } else {
-            console.log("DB contains entry: " + courseResult.userid + " : " + courseResult.courseid + " : " + courseResult.lecture);
+//            console.log("DB contains entry: " + courseResult.userid + " : " + courseResult.courseid + " : " + courseResult.lecture);
+            console.log("DB contains entry: " + courseResult.userid + " : " + courseResult.courseid);
             return res.json({
                 Status: "Failed",
-                Message: "Entry exists in DB"
+                Message: "Entry exists in DB, or there are no available timeslots"
             });
         }
     });
@@ -178,8 +188,9 @@ function insertCourse(req, res) {
 function removeCourse(req, res) {
 
     var userid = req.body.userid;
-    var course = req.body.courseid + req.body.sem;
-    var section = req.body.lecture;
+//    var course = req.body.courseid + req.body.sem;
+//    var section = req.body.lecture;
+    var course = req.body.courseid;
 
     Course.findOneAndRemove({ "userid": userid, "courseid": course }, function(err, id) {
         if (err) {
@@ -271,6 +282,111 @@ function isLoggedIn(req, res, next) {
         Message: "You need to be logged in to access content."
     });
 };
+
+// Adds courses
+// {"code": "", "name": "", "instructor": "", "timeslots": [[day, start]], "time": [start, duration], "colour": "", "timeslots":[[start, end]]}
+function buildTimetable(data) {
+    var startTime;
+    var endTime;
+    var day;
+    var days;
+    var time;
+    var conflict;
+    var timeslots;
+    
+    // Iterate through the meeting sections
+    for (let i = 0; i < data.meeting_sections.length; i++) {
+        days = [];
+        timeslots = [];
+        
+        // Iterate through a meeting section's timeslots
+        conflict = false;
+        startTime = 0;
+        endTime = 0;
+        for (let k = 0; k < data.meeting_sections[i].times.length; k++) {
+
+            // If there is a conflict
+            if (conflict == true) {
+                    break;
+                }
+            
+            // Convert timeslots to numbers
+            startTime = data.meeting_sections[i].times[k].start;
+            endTime = data.meeting_sections[i].times[k].end;
+            endTime /= 3600;
+            startTime /= 3600;
+            startTime += convertDayToNum(data.meeting_sections[i].times[k].day);
+            endTime += convertDayToNum(data.meeting_sections[i].times[k].day);
+            timeslots.push([startTime, endTime]);
+            
+            // Convert timeslots for front end
+            day = abbreviateDay(data.meeting_sections[i].times[k].day);
+            time = data.meeting_sections[i].times[k].start / 3600;
+            days.push([day, time]);
+            
+            // Iterate through the selected courses to check for conflicts
+            for (let m = 0; m < selectedCourses.length; m++) {
+                
+                // If there is a conflict
+                for (let j = 0; j < selectedCourses[m].timeslots[j]; j++) {
+                    if (startTime >= selectedCourses[m].timeslots[j][0] && startTime <= selectedCourses[m].timeslots[1]) {
+                        conflict = true;
+                        break;
+                    }   
+                }
+            }
+        }
+        if (conflict == false) {
+            selectedCourses.push({"code": data.code, "name": data.name, "instructor": data.meeting_sections[i].instructors, "days": days, "timeslots": timeslots, "colour": ""});
+            return true;
+        }
+    }
+    return false;
+}
+
+function convertDayToNum(day) {
+    var num;
+    switch(day) {
+        case "MONDAY":
+            num = 0;
+            break;
+        case "TUESDAY":
+            num = 24;
+            break;
+        case "WEDNESDAY":
+            num = 48;
+            break;
+        case "THURSDAY":
+            num = 72;
+            break;
+        case "FRIDAY":
+            num = 96;
+            break;
+    }
+    return num;
+}
+
+function abbreviateDay(day) {
+    var d;
+    switch(day) {
+        case "MONDAY":
+            d = "M";
+            break;
+        case "TUESDAY":
+            d = "T";
+            break;
+        case "WEDNESDAY":
+            d = "W";
+            break;
+        case "THURSDAY":
+            d = "TH";
+            break;
+        case "FRIDAY":
+            d = "F";
+            break;
+    }
+    return d;
+}
 
 /**
  * Relevant routes for courses & navigating Cobalt.
