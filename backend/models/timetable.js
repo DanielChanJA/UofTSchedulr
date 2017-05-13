8674139fbdf3bf42dd0d17112324cc6495087272
Daniel Chan <iam@danielchan.me>
2017-05-13 18:12:16 -0400

    ejs templates

--- backend/models/timetable.js
+++ backend/models/timetable.js
@@ -1,7 +1,13 @@
 var mongoose = require("mongoose");
+var course = require("course");
 
 var timetableSchema = new mongoose.Schema({
-
+    userid: Number,
+    timetableName: String,
+    createdAt: Date,
+    lastUpdatedAt: Date,
+    note: String,
+    courses: [course]
 });
 
 
