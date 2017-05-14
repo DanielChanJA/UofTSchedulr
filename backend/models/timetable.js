121bb125a2eca9989909431292fe3fde96c346e4
Daniel Chan <iam@danielchan.me>
2017-05-13 21:20:00 -0400

    Timetable updated.

--- backend/models/timetable.js
+++ backend/models/timetable.js
@@ -11,4 +11,16 @@ var timetableSchema = new mongoose.Schema({
 });
 
 
+timetableSchema.statics.createTimetable = function() {
+
+}
+
+timetableSchema.statics.deleteTimetable = function(id) {
+
+}
+
+timetableSchema.statics.getAllTimetablesByUser = function(userid) {
+
+}
+
 module.exports = mongoose.model("timetable", timetableSchema);
